caf14407c49489d7d8e28c6076950450978adbb3
Michael Ferris <mrferris@buffalo.edu>
2013-09-21 04:42:41 -0400

    test

--- src/com/mhacks/HomeActivity.java
+++ src/com/mhacks/HomeActivity.java
@@ -10,6 +10,7 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		System.out.println("yea");
 	}
 
 	@Override
